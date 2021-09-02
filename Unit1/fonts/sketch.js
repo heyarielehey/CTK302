c9775e3e32904c42b182234fba6356d61270d66d
heyarielehey <anjone10@ilstu.edu>
2021-09-02 11:23:59 -0500

    fonty

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,14 +1,19 @@
-var f1 ;
+let f1, f2 ;
 
 
 function setup() {
   createCanvas(500, 500);
   f1 = loadFont('assets/Cabron.ttf') ;
+  f2 = loadFont('assets/goodtimes.ttf') ;
 }
 
 function draw() {
 background ("yellow")
 textFont(f1);
 textSize(48);
-text("hello world" , 100, 100) ;
+text("hello world" , width/2, 100) ;
+
+textFont(f2);
+textSize(24);
+text("goodbye universe", width/2, 160);
 }
