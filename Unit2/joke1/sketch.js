670cd314be8e832ef941bc48f036c22c4b037fd6
heyarielehey <anjone10@ilstu.edu>
2021-09-28 11:15:16 -0500

    so much

---
+++ Unit2/joke1/sketch.js
@@ -0,0 +1,30 @@
+let state = 0;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  switch (state) {
+
+    case 0;
+    background('blue');
+    text("what do you call a sad strawberry?", 100, 100)
+    break;
+
+    case 1:
+      background("yellow");
+      text("blueberry", 100, 100);
+
+      break;
+  }
+
+}
+
+function mouseReleased() {
+  state++
+  if (state > 1) {
+    state = 0;
+  }
+}
+}
---
+++ Unit2/multsound/sketch.js
@@ -0,0 +1,60 @@
+let song1, song2, song3;
+
+
+function preload() {
+  song1 = loadSound("assets/donkey.mp3");
+  song2 = loadSound("assets/skate.mp3");
+  song3 = loadSound("assets/thrill.mp3");
+
+  song1.play();
+  song2.play();
+  song2.play();
+  song1.pause();
+  song2.pause();
+  song3.pause();
+
+}
+
+
+function() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+switch(state) {
+
+  case 0:
+  song1.play() ; // spawn song 1
+  state = 1 ;
+  break;
+
+  case 1: // listen to song 1
+  background('red') ;
+
+  break;
+
+  case 2: // listen to 2
+  song2.play();
+  state = 3 ;
+  break;
+
+  case 3:
+  break;
+
+  case 4:
+  break;
+
+  case 5:
+  background('yellow');
+  break;
+  }
+}
+
+
+function mouseReleased() {
+  song1.pause();
+  song2.pause();
+  song3.pause();
+
+
+}
